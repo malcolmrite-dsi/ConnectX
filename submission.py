12d452a9b1b010adccef3d940328f8edd58a9483
malcolmrite-dsi <malcolm@dsi-program.com>
2021-04-12 20:45:50 +0200

    Second Submission (Mostly from Tut)
    Kaggle Tutorial in Heuristics

--- submission.py
+++ submission.py
@@ -1,66 +1,77 @@
-
-import random
-import numpy as np
-
-
-
 def my_agent(obs, config):
-    # Gets board at next step if agent drops piece in selected column
-    def drop_piece(grid, col, piece, config):
+    import random
+    import numpy as np
+
+    def score_move(grid, col, mark, config):
+        next_grid = drop_piece(grid, col, mark, config)
+        score = get_heuristic(next_grid, mark, config)
+        return score
+
+    # Helper function for score_move: gets board at next step if agent drops piece in selected column
+    def drop_piece(grid, col, mark, config):
         next_grid = grid.copy()
         for row in range(config.rows-1, -1, -1):
             if next_grid[row][col] == 0:
                 break
-        next_grid[row][col] = piece
+        next_grid[row][col] = mark
         return next_grid
 
-    # Returns True if dropping piece in column results in game win
-    def check_winning_move(obs, config, col, piece):
-        # Convert the board to a 2D grid
-        grid = np.asarray(obs.board).reshape(config.rows, config.columns)
-        next_grid = drop_piece(grid, col, piece, config)
+    # Helper function for score_move: calculates value of heuristic for grid
+    def get_heuristic(grid, mark, config):
+        A = 10000
+        B = 100
+        C = 1
+        D = -1
+        E = -1000
+
+        num_twos = count_windows(grid, 2, mark, config)
+        num_threes = count_windows(grid, 3, mark, config)
+        num_fours = count_windows(grid, 4, mark, config)
+        num_twos_opp = count_windows(grid, 2, mark%2+1, config)
+        num_threes_opp = count_windows(grid, 3, mark%2+1, config)
+        score = A*num_fours + B*num_threes + C*num_twos + D*num_twos_opp + E*num_threes_opp
+        return score
+
+    # Helper function for get_heuristic: checks if window satisfies heuristic conditions
+    def check_window(window, num_discs, piece, config):
+        return (window.count(piece) == num_discs and window.count(0) == config.inarow-num_discs)
+
+    # Helper function for get_heuristic: counts number of windows satisfying specified heuristic conditions
+    def count_windows(grid, num_discs, piece, config):
+        num_windows = 0
         # horizontal
         for row in range(config.rows):
             for col in range(config.columns-(config.inarow-1)):
-                window = list(next_grid[row,col:col+config.inarow])
-                if window.count(piece) == config.inarow:
-                    return True
+                window = list(grid[row, col:col+config.inarow])
+                if check_window(window, num_discs, piece, config):
+                    num_windows += 1
         # vertical
         for row in range(config.rows-(config.inarow-1)):
             for col in range(config.columns):
-                window = list(next_grid[row:row+config.inarow,col])
-                if window.count(piece) == config.inarow:
-                    return True
+                window = list(grid[row:row+config.inarow, col])
+                if check_window(window, num_discs, piece, config):
+                    num_windows += 1
         # positive diagonal
         for row in range(config.rows-(config.inarow-1)):
             for col in range(config.columns-(config.inarow-1)):
-                window = list(next_grid[range(row, row+config.inarow), range(col, col+config.inarow)])
-                if window.count(piece) == config.inarow:
-                    return True
+                window = list(grid[range(row, row+config.inarow), range(col, col+config.inarow)])
+                if check_window(window, num_discs, piece, config):
+                    num_windows += 1
         # negative diagonal
         for row in range(config.inarow-1, config.rows):
             for col in range(config.columns-(config.inarow-1)):
-                window = list(next_grid[range(row, row-config.inarow, -1), range(col, col+config.inarow)])
-                if window.count(piece) == config.inarow:
-                    return True
-        return False
+                window = list(grid[range(row, row-config.inarow, -1), range(col, col+config.inarow)])
+                if check_window(window, num_discs, piece, config):
+                    num_windows += 1
+        return num_windows
 
 
-    # Your code here: Amend the agent!
     valid_moves = [col for col in range(config.columns) if obs.board[col] == 0]
-    # Your code here: Amend the agent!
-    piece=obs.mark
-    for col in valid_moves:
-        if check_winning_move(obs, config, col, piece):
-            return col
-
-    if obs.mark == 1:
-        piece = 2
-    else:
-        piece = 1
-
-    for col in valid_moves:
-        if check_winning_move(obs, config, col, piece):
-            return col
-
-    return random.choice(valid_moves)
+    # Convert the board to a 2D grid
+    grid = np.asarray(obs.board).reshape(config.rows, config.columns)
+    # Use the heuristic to assign a score to each possible board in the next turn
+    scores = dict(zip(valid_moves, [score_move(grid, col, obs.mark, config) for col in valid_moves]))
+    # Get a list of columns (moves) that maximize the heuristic
+    max_cols = [key for key in scores.keys() if scores[key] == max(scores.values())]
+    # Select at random from the maximizing columns
+    return random.choice(max_cols)
